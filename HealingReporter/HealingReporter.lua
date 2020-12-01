513dab6c391bf10e3ddba4a788b1eb84c062c292
scythae <scythae@mail.ru>
2020-12-01 04:45:33 +0300

    Healing Reporter. Addon is extended for showing also damage output.

--- HealingReporter/HealingReporter.lua
+++ HealingReporter/HealingReporter.lua
@@ -1,207 +1,209 @@
---mop-veins.tk, 29.09.2020
+-- mop-veins.tk, 07.11.2020
+-- Use ingame chat command "/reload" after changing settings 
+local Preset = {}
+Preset.DefaultFont = "GameFontNormalHuge3" -- list is here https://www.townlong-yak.com/framexml/live/SharedFontStyles.xml
+Preset.ColorRGB_Green = {0.2, 1, 0.2}
+Preset.ColorRGB_Yellow = {1, 0.82, 0}
+Preset.ColorRGB_Gray = {0.8, 0.8, 0.8}
 
-local Settings = {}
--- after changing settings use ingame chat command /reload
-Settings.Position = {500, 0}
-Settings.ReportInterval_Sec = 3
-Settings.VSpeed_PxPerSec = 50
-Settings.TravelHeight = 200
-Settings.LineHeight = 32
-Settings.ColorRGB_EfficientHeal = {0.2, 1, 0.2}
-Settings.ColorRGB_OverHeal = {1, 0.82, 0}
-Settings.FontStyle = "GameFontNormalHuge3" -- list is here https://www.townlong-yak.com/framexml/live/SharedFontStyles.xml
-Settings.ShortNumbers = true
+local SettingsHeal = {
+	Position = {500, 0},
+	ReportInterval_Sec = 3,
+	VSpeed_PxPerSec = 70,
+	TravelHeight = 200,
+	LineHeight = 32,
+	ColorRGB_EfficientAmount = Preset.ColorRGB_Green,
+	ColorRGB_ExcessiveAmount = Preset.ColorRGB_Yellow,
+	FontStyle = Preset.DefaultFont,
+	ShortNumbers = true
+}
+local SettingsDamage = {
+	Position = {-470, 0},
+	ReportInterval_Sec = 3,
+	VSpeed_PxPerSec = 70,
+	TravelHeight = 180,
+	LineHeight = 24,
+	ColorRGB_EfficientAmount = Preset.ColorRGB_Yellow,
+	ColorRGB_ExcessiveAmount = Preset.ColorRGB_Gray,
+	FontStyle = Preset.DefaultFont,
+	ShortNumbers = true
+}
 
-local HR = CreateFrame("Frame", nil, UIParent)
-HR:SetFrameStrata("BACKGROUND")
-HR:SetPoint("CENTER", unpack(Settings.Position))
-HR:SetSize(10, 10)
-HR:Show()
 
-local Lines = {}
-local LinesPool = {}
-local Report = {}
 
-local ElapsedSinceLastReport = 0
+local MainFrame = CreateFrame("Frame", nil, UIParent)
+MainFrame:SetFrameStrata("BACKGROUND")
+MainFrame:SetPoint("CENTER")
+MainFrame:SetSize(10, 10)
+MainFrame:Show()
 
-local function HR_CreateNewLine()
-	local Line = HR:CreateFontString(nil, "OVERLAY", Settings.FontStyle)
-	Line:SetTextColor(unpack(Settings.ColorRGB_EfficientHeal))
-	Line:SetTextHeight(Settings.LineHeight)
+local ReportBlock = {}
+function ReportBlock:new(Settings)
+	local res = {}
+	setmetatable(res, self)
+	self.__index = self
+	res.Settings = Settings or SettingsHeal
+	res.Lines = {}
+	res.LinesPool = {} 
+	res.Report = {}
+	res.ElapsedSinceLastReport = 0
 
-	local Icon = HR:CreateTexture(nil, "OVERLAY")
+	local Anchor = CreateFrame("Frame", nil, MainFrame)
+	Anchor:SetFrameStrata("BACKGROUND")
+	Anchor:SetPoint("CENTER", unpack(res.Settings.Position))
+	Anchor:SetSize(10, 10)
+	Anchor:Show()
+	res.Anchor = Anchor
+
+	return res
+end
+
+function ReportBlock:CreateNewLine()
+	local S = self.Settings
+	local Anchor = self.Anchor
+
+	local Line = Anchor:CreateFontString(nil, "OVERLAY", S.FontStyle)
+	Line:SetTextColor(unpack(S.ColorRGB_EfficientAmount))
+	Line:SetTextHeight(S.LineHeight)
+
+	local Icon = Anchor:CreateTexture(nil, "OVERLAY")
 	Icon:SetPoint("LEFT", Line, "RIGHT", 0, 0)
-	Icon:SetSize(Settings.LineHeight, Settings.LineHeight)
+	Icon:SetSize(S.LineHeight, S.LineHeight)
 	Line.Icon = Icon
 
-	local LinePart_Overheal = HR:CreateFontString(nil, "OVERLAY", Settings.FontStyle)
-	LinePart_Overheal:SetTextColor(unpack(Settings.ColorRGB_OverHeal))
-	LinePart_Overheal:SetTextHeight(Settings.LineHeight)
-	LinePart_Overheal:SetPoint("LEFT", Icon, "RIGHT", 0, 0)
-	Line.LinePart_Overheal = LinePart_Overheal
+	local ExtraLine = Anchor:CreateFontString(nil, "OVERLAY", S.FontStyle)
+	ExtraLine:SetTextColor(unpack(S.ColorRGB_ExcessiveAmount))
+	ExtraLine:SetTextHeight(S.LineHeight)
+	ExtraLine:SetPoint("LEFT", Icon, "RIGHT", 0, 0)
+	Line.ExtraLine = ExtraLine
 
 	return Line
 end
 
-local function HR_GetFreshLine()
+function ReportBlock:GetFreshLine()
 	local Line
-	if #LinesPool > 0 then
-		Line = table.remove(LinesPool)
+	if #(self.LinesPool) > 0 then
+		Line = table.remove(self.LinesPool)
 	else
-		Line = HR_CreateNewLine()
+		Line = self:CreateNewLine()
 	end
 
-	local yOffset = 0
-	local PrevLine = Lines[1]
+	local y = 0
+	local PrevLine = self.Lines[1]
 	if PrevLine then
-		local PrevLineY = ({PrevLine:GetPoint()})[5]
-		if PrevLineY > -Settings.LineHeight then
-			yOffset = PrevLineY + Settings.LineHeight
+		local yPrevLine = ({PrevLine:GetPoint()})[5]
+		local yTmp = yPrevLine + self.Settings.LineHeight
+		if yTmp > 0 then
+			y = yTmp
 		end
-	end		
-	Line:SetPoint("RIGHT", 0, yOffset)
+	end
 	
-	table.insert(Lines, 1, Line)
+	Line:SetPoint("RIGHT", 0, y)
+	
+	table.insert(self.Lines, 1, Line)
 	return Line
 end
 
-local function HR_FormatNumber(Val)
-	if Settings.ShortNumbers and Val > 1000000 then
+function ReportBlock:FormatNumber(Val)
+	local ShortNumbers = self.Settings.ShortNumbers
+
+	if ShortNumbers and Val > 1000000 then
 		return string.format("%.1fkk", Val / 1000000)
 	end
 
-	if Settings.ShortNumbers and Val > 1000 then
+	if ShortNumbers and Val > 1000 then
 		return string.format("%.1fk", Val / 1000)
 	end
 
 	return Val
 end
 
-local function HR_Report()	
-	local SpellId, HealAmounts
-	for SpellId, HealAmounts in pairs(Report)
+function ReportBlock:DoReport()	
+	local SpellId, Amounts
+	for SpellId, Amounts in pairs(self.Report)
 	do
-		local Line = HR_GetFreshLine()
+		local Line = self:GetFreshLine()
 		Line.Icon:SetTexture(GetSpellTexture(SpellId))
 		
-		local EfficientHeal, Overheal = unpack(HealAmounts)
-		if EfficientHeal > 0 then
-			Line:SetText(HR_FormatNumber(EfficientHeal))
+		local EfficientAmount, ExcessiveAmount = unpack(Amounts)
+		if EfficientAmount > 0 then
+			Line:SetText(self:FormatNumber(EfficientAmount))
 		end	
-		if Overheal > 0 then
-			Line.LinePart_Overheal:SetText(HR_FormatNumber(Overheal))
+		if ExcessiveAmount > 0 then
+			Line.ExtraLine:SetText(self:FormatNumber(ExcessiveAmount))
 		end
 	end
 
-	Report = {}
+	self.Report = {}
 end
 
-local function HR_OnUpdate(Self, Elapsed)	
-	local VShift = Elapsed * Settings.VSpeed_PxPerSec
-
-	ElapsedSinceLastReport = ElapsedSinceLastReport + Elapsed
-	if ElapsedSinceLastReport > Settings.ReportInterval_Sec then
-		HR_Report()
-		ElapsedSinceLastReport = 0
+function ReportBlock:TryReport(Elapsed)
+	self.ElapsedSinceLastReport = self.ElapsedSinceLastReport + Elapsed
+	if self.ElapsedSinceLastReport > self.Settings.ReportInterval_Sec then
+		self:DoReport()
+		self.ElapsedSinceLastReport = 0
 	end
+end
+
+function ReportBlock:SlideLines(Elapsed)
+	local Lines = self.Lines
+	local yOffset = Elapsed * self.Settings.VSpeed_PxPerSec
 
 	local i, Line
 	for i = #Lines, 1, -1
 	do
 		Line = Lines[i]
-		point, relativeTo, relativePoint, xOfs, yOfs = Line:GetPoint()
-		yOfs = yOfs - VShift
-		if yOfs > -Settings.TravelHeight then
-			Line:SetPoint(point, relativeTo, relativePoint, xOfs, yOfs)
+		point, relativeTo, relativePoint, x, y = Line:GetPoint()
+		y = y - yOffset
+		local CanSlideFurther = y >  -self.Settings.TravelHeight
+		if CanSlideFurther then
+			Line:SetPoint(point, relativeTo, relativePoint, x, y)
 		else
 			Line:SetText("")
-			Line.LinePart_Overheal:SetText("")
+			Line.ExtraLine:SetText("")
 			Line.Icon:SetTexture(nil)
 			table.remove(Lines)
-			table.insert(LinesPool, Line)
+			table.insert(self.LinesPool, Line)
 		end		
 	end
 end
-HR:SetScript("OnUpdate", HR_OnUpdate);
+
+function ReportBlock:OnUpdate(Elapsed)
+	self:TryReport(Elapsed)
+	self:SlideLines(Elapsed)
+end
+
+function ReportBlock:AddToReport(SpellId, TotalAmount, ExcessiveAmount)
+	local EfficientAmount = TotalAmount - ExcessiveAmount
+	local ReportValues = self.Report[SpellId] or {0, 0}
+	ReportValues[1] = ReportValues[1] + EfficientAmount
+	ReportValues[2] = ReportValues[2] + ExcessiveAmount
+	self.Report[SpellId] = ReportValues
+end
+
+local RB_Heal = ReportBlock:new(SettingsHeal)
+local RB_Damage = ReportBlock:new(SettingsDamage)
+
+local function HR_OnUpdate(Self, Elapsed)
+	RB_Heal:OnUpdate(Elapsed)
+	RB_Damage:OnUpdate(Elapsed)
+end
+MainFrame:SetScript("OnUpdate", HR_OnUpdate);
 
 local function HR_OnEvent(
-	Self, Event, a1, EventType, _, _, SourceName, a6, _, _, 
-	TargetName, a10, a11, SpellId, SpellName, _, Amount, Overheal, a17, a18,
+	Self, Event, a1, EventType, a3, a4, SourceName, a6, a7, a8, 
+	TargetName, a10, a11, SpellId, SpellName, a14, TotalAmount, ExcessiveAmount, a17, a18,
 ...)
-	local Healing = EventType == "SPELL_HEAL";
-	local HealingPeriodic = EventType == "SPELL_PERIODIC_HEAL";
-	local PlayerHeals = (Healing or HealingPeriodic) and SourceName 
-		and UnitIsUnit(SourceName, "Player");
+	local ActionsOfPlayer = SourceName and UnitIsUnit(SourceName, "Player")
+	if not ActionsOfPlayer then return end	
 
-	if not PlayerHeals then return end
+	if EventType == "SPELL_HEAL" or EventType == "SPELL_PERIODIC_HEAL" then
+		RB_Heal:AddToReport(SpellId, TotalAmount, ExcessiveAmount)
+	end
 
-	local EfficientHeal = Amount - Overheal
-
-	local ReportValues = Report[SpellId] or {0, 0}
-	ReportValues[1] = ReportValues[1] + EfficientHeal
-	ReportValues[2] = ReportValues[2] + Overheal
-	Report[SpellId] = ReportValues
+	if EventType == "SPELL_DAMAGE" or EventType == "SPELL_PERIODIC_DAMAGE" then		
+		RB_Damage:AddToReport(SpellId, TotalAmount, ExcessiveAmount)
+	end
 end
-HR:SetScript("OnEvent", HR_OnEvent);
-HR:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
-
-
-local function RunTests()
-	local t = {}
-	local passed = true
-	local str
-	
-	table.insert(t, "asd");
-	table.insert(t, "qwe");
-	passed = passed and #t == 2 and t[1] == "asd" and t[2] == "qwe";
-	
-	str = ""
-	local i
-	for i = #t, 1, -1
-	do
-		str = str..t[i]
-	end
-	passed = passed and str == "qweasd"
-	
-	str = table.remove(t);
-	passed = passed and #t == 1 and str == "qwe";
-	
-	str = table.remove(t);
-	passed = passed and #t == 0 and str == "asd";
-	
-	table.insert(t, "asd");
-	table.insert(t, "qwe");
-	table.insert(t, 1, "zxc")
-	passed = passed and #t == 3 and t[1] == "zxc" and t[3] == "qwe"
-	
-	local function TestArray()
-		return "qwe", "asd", "zxc"
-	end
-	t = {TestArray()}
-	passed = passed and #t == 3 and t[1] == "qwe" and t[3] == "zxc"
-	
-	local function TestUnpack(a, b, c)
-		return a == 1 and b == 2 and c == 3
-	end
-	passed = passed and TestUnpack(unpack({1, 2, 3}))
-	
-	passed = passed and ({"a", "s", "d"})[3] == "d"
-	
-	--sparsed array
-	t = {}
-	t[123] = "a"
-	t[234] = "b"
-	str = ""
-	local key, val
-	for key, val in pairs(t)
-	do
-		str = str..key..val
-	end
-	passed = passed and str == "123a234b"
-	
-	
-	print("passed: "..tostring(passed));
-end
-
-RunTests()
-
+MainFrame:SetScript("OnEvent", HR_OnEvent)
+MainFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
---
+++ HealingReporter/HealingReporterTests.lua
@@ -0,0 +1,71 @@
+local function RunTests()
+	--table's basic tests
+	local t = {}
+	local passed = true
+	local str
+	
+	table.insert(t, "asd");
+	table.insert(t, "qwe");
+	passed = passed and #t == 2 and t[1] == "asd" and t[2] == "qwe";
+	
+	str = ""
+	local i
+	for i = #t, 1, -1
+	do
+		str = str..t[i]
+	end
+	passed = passed and str == "qweasd"
+	
+	str = table.remove(t);
+	passed = passed and #t == 1 and str == "qwe";
+	
+	str = table.remove(t);
+	passed = passed and #t == 0 and str == "asd";
+	
+	table.insert(t, "asd");
+	table.insert(t, "qwe");
+	table.insert(t, 1, "zxc")
+	passed = passed and #t == 3 and t[1] == "zxc" and t[3] == "qwe"
+	
+	local function TestArray()
+		return "qwe", "asd", "zxc"
+	end
+	t = {TestArray()}
+	passed = passed and #t == 3 and t[1] == "qwe" and t[3] == "zxc"
+	
+	local function TestUnpack(a, b, c)
+		return a == 1 and b == 2 and c == 3
+	end
+	passed = passed and TestUnpack(unpack({1, 2, 3}))
+	
+	passed = passed and ({"a", "s", "d"})[3] == "d"
+	
+	--sparsed array
+	t = {}
+	t[123] = "a"
+	t[234] = "b"
+	str = ""
+	local key, val
+	for key, val in pairs(t)
+	do
+		str = str..key..val
+	end
+	passed = passed and str == "123a234b"
+	
+	--oop
+	local obj = {val = 10}
+	function obj:TestVal()		
+		return val == nil and self.Val == nil and self.val == 10
+	end
+	passed = passed and obj:TestVal() 
+	
+	function TestVariableArgs(...)
+		local arg = {...}
+		return arg[2] == "asd"
+	end
+	passed = passed and TestVariableArgs(nil, "asd", 30) 
+
+	print("Healing Reporter tests passed: "..tostring(passed));
+end
+
+RunTests()
