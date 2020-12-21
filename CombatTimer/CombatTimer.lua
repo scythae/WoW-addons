c36f7446d47d224eb23aa62a6772e16daeea0d00
scythae <scythae@mail.ru>
2020-12-21 19:53:27 +0300

    CombatTimer. Now it consumes less CPU, but tracks only bossfights, without regular combat.

--- CombatTimer/CombatTimer.lua
+++ CombatTimer/CombatTimer.lua
@@ -1,38 +1,53 @@
-local CT = CreateFrame("Frame", nil, UIParent);
-CT:SetFrameStrata("BACKGROUND");
-CT:SetWidth(128);
-CT:SetHeight(64); 
-CT:SetPoint("CENTER", 0, 400);
-CT.Text = CT:CreateFontString(nil, "OVERLAY", "GameFontHighlightLarge");
-CT.Text:SetAllPoints(CT);
-CT:Show();
+local A = {}
 
+A.Run = function()
+	local f = CreateFrame("Frame", nil, UIParent)
+	f:SetFrameStrata("BACKGROUND")
+	f:SetSize(128, 64)
+	f:SetPoint("CENTER", 0, 400)
 
-local WhenCombatStarted = GetTime();
-local LastStateWasCombat = true;
-local LastSecondsElapsed;
+	A.Frame = f
 
-local function AnyOfMembersInCombat()
-	local raidIndex, memberName;
-	for raidIndex = 1, 40, 1 do
-		memberName = GetRaidRosterInfo(raidIndex);
-		if memberName == nil then
-			if raidIndex == 1 then
-				memberName = "player";
-			else
-				break;
-			end
-		end
-			
-		if UnitAffectingCombat(memberName) then
-			return true;
-		end
-	end
-	
-	return false;
+	A.Text = f:CreateFontString(nil, "OVERLAY", "GameFontHighlightLarge")
+	A.Text:SetAllPoints(f)
+
+	f:SetScript("OnEvent", A.OnEvent)
+	f:RegisterEvent("ENCOUNTER_START")
+	f:RegisterEvent("ENCOUNTER_END")	
 end
 
-local function ensureTwoDigits(num)
+A.OnEvent = function(Self, Event)
+	if Event == "ENCOUNTER_START" then
+		A.WhenCombatStarted = GetTime()
+		A.Brake = 0
+		A.Frame:SetScript("OnUpdate", A.OnUpdate)
+	end
+
+	if Event == "ENCOUNTER_END" then
+		A.Frame:SetScript("OnUpdate", nil)
+	end
+end
+
+A.OnUpdate = function(Self, Elapsed)	
+	A.Brake = A.Brake + Elapsed
+	if A.Brake < 1 then return end
+	A.Brake = A.Brake - 1
+
+	local SecondsElapsed = math.floor(GetTime() - A.WhenCombatStarted)
+	
+	local h, m, s;
+	
+	h = math.floor(SecondsElapsed / 3600);
+	h = A.EnsureTwoDigits(h);
+	m = math.floor(SecondsElapsed / 60);
+	m = A.EnsureTwoDigits(m);
+	s = SecondsElapsed - m * 60;	
+	s = A.EnsureTwoDigits(s);
+	
+	A.Text:SetText(h..":"..m..":"..s);
+end
+
+A.EnsureTwoDigits = function(num)
 	if num < 10 then
 		return "0"..num;
 	else
@@ -40,40 +55,7 @@ local function ensureTwoDigits(num)
 	end	
 end
 
-local function CombatTimer_OnUpdate(Self, Elapsed)	
-	local StateIsCombat = AnyOfMembersInCombat();
-	
-	if not StateIsCombat then
-		if LastStateWasCombat then			
-			LastStateWasCombat = false;
-			LastSecondsElapsed = -1;
-		end
-		
-		return;
-	end
-	
-	if not LastStateWasCombat then
-		LastStateWasCombat = true;
-		WhenCombatStarted = GetTime();
-	end
 
-	local SecondsElapsed = math.floor(GetTime() - WhenCombatStarted);
-	if SecondsElapsed == LastSecondsElapsed then
-		return;
-	end
-	
-	LastSecondsElapsed = SecondsElapsed;
-	
-	local h, m, s;
-	
-	h = math.floor(SecondsElapsed / 3600);
-	h = ensureTwoDigits(h);
-	m = math.floor(SecondsElapsed / 60);
-	m = ensureTwoDigits(m);
-	s = SecondsElapsed - m * 60;	
-	s = ensureTwoDigits(s);
-	
-	CT.Text:SetText(h..":"..m..":"..s);
-end
 
-CT:SetScript("OnUpdate", CombatTimer_OnUpdate);
+
+A.Run()
