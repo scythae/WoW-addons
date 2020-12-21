f9dea002390b7761568168e0249de71fb09b8122
scythae <scythae@mail.ru>
2020-12-22 02:26:13 +0300

    DelayScript. Reworked for usage of new "Delay" addon.

--- DelayScript/DelayScript.lua
+++ DelayScript/DelayScript.lua
@@ -1,122 +1,31 @@
---mop-veins.tk, 10.04.2019
+-- tauriwow.com, 2020-12-21
 
-local AddonName = "DelayScript";
-local DelaySlashCommand = "/delayscript";
-local Schedule = {};
+local AddonName = ...
+local SlashCommand = "/delayscript"
 
-local function Print(Text)
-	DEFAULT_CHAT_FRAME:AddMessage(AddonName..". "..tostring(Text));
-end;
-
-local function Trim(Text, TrimChar)
-	if not Text then return; end;
-	TrimChar = TrimChar or " ";
-	return string.gsub(Text, "^["..TrimChar.."]*(.-)["..TrimChar.."]*$", "%1", 1);
-end;
-
-local function CutLexem(Text, Delimiter)
-	if not strfind(Text, Delimiter) then
-		return Text, nil;
-	end;
-	
-	Text = Trim(Text, Delimiter);	
-	local Lexem = string.gsub(Text, "^(.-)["..Delimiter.."]+.*$", "%1");
-	local RestText = string.gsub(Text, "^.-["..Delimiter.."]+(.*)$", "%1");
-	
-	return Lexem, RestText; 
-end;
-
-local function PutDelayedScript(DelayInSeconds, ScriptText)
-	local WhenExecute = GetTime() + tonumber(DelayInSeconds);
-	
-	local Task = {};
-	Task.Time = WhenExecute;
-	Task.Script = ScriptText;
-	
-	table.insert(Schedule, Task);
-end;
-
-local function ScheduleScript(CommandText)
-	local DelayInSeconds, ScriptText = CutLexem(CommandText, " "); 
-	if not DelayInSeconds or not ScriptText then
-		return;
-	end;
-	
-	PutDelayedScript(DelayInSeconds, ScriptText);
-end;
-
-local function RegisterSlashCommand()
-	SlashCmdList[AddonName] = function(command)
-		ScheduleScript(command);
-	end;
-	SLASH_DelayScript1 = DelaySlashCommand; 
-end;
-
-RegisterSlashCommand();	
-Print("Is loaded.");
-
-local function RunScheduledScripts()
-	if #Schedule == 0 then
-		return;
-	end;	
-	
-	local Now = GetTime();
-	local CountOfPassed = 0;
-	local index, Task;
-	
-	for index, Task in ipairs(Schedule) do
-		if Task.Time < Now then
-			CountOfPassed = CountOfPassed + 1;	
-			
-			xpcall(
-				function()
-					RunScript(Task.Script);
-				end,
-				function()
-					Print("Failure of executing script. "..Task.Script);
-				end
-			);
-		end;
-	end;	
-	
-	while (CountOfPassed > 0) do 
-		table.remove(Schedule, CountOfPassed);
-		CountOfPassed = CountOfPassed - 1; 
-	end;
-end;
-
-local ElapsedSinceLastCheck = 0;
-local function DelayScript_OnUpdate(Self, Elapsed)
-	if ElapsedSinceLastCheck < 0.3 then
-		ElapsedSinceLastCheck = ElapsedSinceLastCheck + Elapsed;
-		return;
-	end;
-	ElapsedSinceLastCheck = 0;	
-	
-	RunScheduledScripts();
+local function ExecuteScript(st)
+	RunScript(st)
 end
 
-function dsdebug_print(text)
-	Print(text);
-end;
+local function ScheduleScript(CommandText)
+	local DelayInSeconds, ScriptText = strsplit(" ", CommandText, 2)
+	DelayInSeconds = tonumber(DelayInSeconds)
 
-local function NotifyAboutPull(Text)
-	SendChatMessage(Text, "Say", nil, nil);
-end;
+	if not(
+		type(DelayInSeconds) == "number" 
+		and type(ScriptText) == "string"
+	) then	
+		local err = "Usage: %s DelayInSeconds ScriptText"
+		err = string.format(err, SlashCommand)
+		error(err, 2)
+	end
 
-function ds_pull(SecondsRemained)
-	local DelayBeforeNextWarning = 1;
-	local Message;	
+	Delay(DelayInSeconds, ExecuteScript, {ScriptText})
+end
 
-	if SecondsRemained > 0 then
-		PutDelayedScript(DelayBeforeNextWarning, "ds_pull("..(SecondsRemained - DelayBeforeNextWarning)..")");
-		Message = "Pull in "..SecondsRemained;
-	else
-		Message = "Go";
-	end;
-	
-	NotifyAboutPull(Message);
-end;
+local function RegisterSlashCommand()
+	_G["SLASH_"..AddonName.."1"] = SlashCommand
+	SlashCmdList[AddonName] = ScheduleScript
+end
 
-local DS = CreateFrame("Frame", nil, UIParent);
-DS:SetScript("OnUpdate", DelayScript_OnUpdate);
+RegisterSlashCommand()
